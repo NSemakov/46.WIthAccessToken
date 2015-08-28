27a6fbe6abbfa90d84e6205b0b84eaa44fcb38a4
Nikita Semakov <n.semakov@yahoo.com>
2015-08-28 12:27:26 +0300

    Initial Commit

---
+++ 45. APIWithoutAccessToken/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  45. APIWithoutAccessToken
+//
+//  Created by Admin on 28.08.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
